ea4a27d2cad54ac5722de7de500bb35a97ace41f
MatveyVue <shabanov.matveyka@bk.ru>
2024-09-30 20:37:07 +0300

    Update TaskView.vue

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -3,8 +3,7 @@
 
 <center>
 <a href="https://t.me/stinsik"><button   id="clickButton" 
-  :disabled="buttonClicked" 
-  @click="gotoChannel()" class="task1">{{ buttonClicked ? 'Completed✅' }}<h3>Go to Community</h3><h4>+25,000 Cucumber</h4>
+@click="gotoChannel()" class="task1">{{ buttonClicked ? 'Completed✅' }}<h3>Go to Community</h3><h4>+25,000 Cucumber</h4>
 <img class="cucu" src="https://i.postimg.cc/L8h57Z2R/IMG-0722.png" width="50px" height="50px">
 </button></a>
 </center>
